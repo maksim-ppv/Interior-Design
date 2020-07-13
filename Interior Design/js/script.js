5c295b7aa18e700cb7fdddf800afdfddfeb659ec
maksim-ppv <popovm1502@gmail.com>
2020-07-13 18:26:04 +0300

    3 блок

--- Interior Design/js/script.js
+++ Interior Design/js/script.js
@@ -2712,9 +2712,9 @@ const timeout = 800;
 if (popupLinks.length > 0) {
 	for (let index = 0; index < popupLinks.length; index++) {
         const popupLink = popupLinks[index];
-        console.log('popupLink: ', popupLink);
 		popupLink.addEventListener("click", function (e) {
 			const popupName = popupLink.getAttribute('href').replace('#', '');
+			console.log('popupName: ', popupName);
 			const curentPopup = document.getElementById(popupName);
 			popupOpen(curentPopup);
 			e.preventDefault();
@@ -2826,6 +2826,105 @@ document.addEventListener('keydown', function (e) {
 	}
 })();
 
+
+
+;
+
+var isMobile = { Android: function () { return navigator.userAgent.match(/Android/i); }, BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); }, iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i); }, Opera: function () { return navigator.userAgent.match(/Opera Mini/i); }, Windows: function () { return navigator.userAgent.match(/IEMobile/i); }, any: function () { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); } };
+
+const popupFormname = document.querySelector('.popup__form-name');
+const popupThanksTitle = document.querySelector('.popup-thanks__title');
+
+
+
+//FORMS
+function forms(){
+	//OPTION
+	$.each($('.option.active'), function(index, val) {
+		$(this).find('input').prop('checked', true);
+	});
+	$('.option').click(function(event) {
+		if(!$(this).hasClass('disable')){
+				var target = $(event.target);
+			if (!target.is("a")){
+				if($(this).hasClass('active') && $(this).hasClass('order') ){
+					$(this).toggleClass('orderactive');
+				}
+					$(this).parents('.options').find('.option').removeClass('active');
+					$(this).toggleClass('active');
+					$(this).children('input').prop('checked', true);
+			}
+		}
+	});
+}
+forms();
+
+
+//VALIDATE FORMS
+$('form button[type=submit]').click(function(){
+	var er=0;
+	var form=$(this).parents('form');
+	var ms=form.data('ms');
+$.each(form.find('.req'), function(index, val) {
+	er+=formValidate($(this));
+});
+if(er==0){
+	removeFormError(form);
+	if(ms!=null && ms!=''){
+		showMessageByClass(ms);
+		return false;
+	}
+}else{
+	return false;
+}
+});
+function formValidate(input){
+	var er=0;
+	var form=input.parents('form');
+	if(input.val()=='' || input.val()==input.attr('data-value')){
+		er++;
+		addError(input);
+	}else{
+		removeError(input);
+	}
+	return er;
+}
+
+function showMessageByClass(ms){
+
+    if(popupFormname.value == ''){
+	const name = `Спасибо!`;
+	popupThanksTitle.textContent = name;
+	}
+	if(!popupFormname.value == ''){
+	const name = `Спасибо, ${popupFormname.value}!`;
+	popupThanksTitle.textContent = name;
+	}
+	const msPopup = document.getElementById(ms);
+	popupOpen(msPopup);
+}
+
+
+function addError(input){
+	input.addClass('err');
+}
+function removeFormError(form){
+form.find('.form__generalerror').hide().html('');
+}
+function removeError(input){
+input.removeClass('err');
+}
+
+function maskclear(n){
+if(n.val()==""){
+	n.inputmask('remove');
+	if(!n.hasClass('l')){
+		n.val(n.attr('data-value'));
+	}
+	n.removeClass('focus');
+	n.parent().removeClass('focus');
+}
+}
 ;
 
 
